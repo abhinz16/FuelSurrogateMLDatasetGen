a62c1fbab41539d9ff76ff5377b3d2b2412e24c2
abhinz16 <akayam2@uic.edu>
2025-04-15 18:41:48 -0500

    Update main.py

--- Dataset_generator/main.py
+++ Dataset_generator/main.py
@@ -7,10 +7,10 @@ PROCESS FLOW:
 2) Perform fragmentation of palette components to get FG counts.
 3) Generate surrogate mixtures.
 4) Post processing of the generated mixtures to ensure correct mixture ids.
-5) Remove temperorly created files.
+5) Remove temporarily created files.
 6) Generating a csv file combining the FGs of mixtures for statistical analysis later.
 7) Create FG distribution plots.
-8) Correaltion and entropy analysis --> how does the metrics change with number oc components and number of mixtures.
+8) Correlation  and entropy analysis --> how does the metrics change with the number of components and number of mixtures.
 9) Monte-Carlo analysis (irrespective of number of components) --> how does correlation and entropy vary with number of mixtures.
 10) Find the minimum number of mixtures for an optimal dataset. Threshold provided by the user as an input at this step.
 11) Generate optimal dataset for the minimum number of mixtures identified using Monte-Carlo analysis.
